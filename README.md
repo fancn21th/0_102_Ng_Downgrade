3250513a663742d3395066b9c3d7e3de1b6478d1
alex.fan <alex.fan@okchem.com>
2019-08-26 10:11:33 +0800

    fix bug of entry component setting

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Angular Downgrade
 
 - a demo of angular 2+ downgrading to angular 1 directive
+  - refer to `https://plnkr.co/edit/uILrai13oZmPsJxMghAG?p=preview`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,10 @@ import { ArticleComponent } from "./article/article.component";
 @NgModule({
   declarations: [AppComponent, ArticleComponent],
   imports: [BrowserModule, AppRoutingModule, UpgradeModule],
+  // exports: [ArticleComponent],
   providers: [],
+  // IT IS A MUST TO PUT THE DOWNGRADE COMPONENT IN THE ENTRY COMPONENT DEFINITION
+  entryComponents: [ArticleComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
